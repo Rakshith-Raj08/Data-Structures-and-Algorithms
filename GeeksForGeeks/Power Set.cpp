fa63cdc49f54595c814dec1ed39aac7142d038f9
Rakshith-Raj08 <rakshith2002raj@gmail.com>
2024-02-26 21:54:17 +0530

    .

---
+++ GeeksForGeeks/Power Set.cpp
@@ -0,0 +1,22 @@
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+		    vector<string>ans;
+		    int n=s.size();
+		    for(int i=1;i<(1<<n);i++)
+		    {
+		        string str="";
+		        for(int j=0;j<n;j++)
+		        {
+		            if(i&(1<<j))
+		            {
+		                str+=s[j];
+		            }
+		        }
+		        ans.push_back(str);
+		    }
+		   sort(ans.begin(),ans.end());
+		    return ans;
+		}
+};
