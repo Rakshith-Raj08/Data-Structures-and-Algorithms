eb4bb3ed4c3e8eeb3c2c22f699b6bbd0423c6dc6
Rakshith-Raj08 <rakshith2002raj@gmail.com>
2024-03-18 18:27:44 +0530

    .

---
+++ GeeksForGeeks/Level order traversal.cpp
@@ -0,0 +1,27 @@
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* root)
+    {
+      //Your code here
+      vector<int>v;
+      queue<Node*>q;
+      q.push(root);
+      while(q.size()>0){
+          Node* curr=q.front();
+          q.pop();
+          v.push_back(curr->data);
+          if(curr->left!=NULL){
+              q.push(curr->left);
+          }
+          if(curr->right!=NULL){
+              q.push(curr->right);
+          }
+      }
+      return v;
+      
+    }
+
+};
