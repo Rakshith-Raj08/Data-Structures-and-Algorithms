fc48530add280bc43931060279bf6635dea4d019
Rakshith-Raj08 <rakshith2002raj@gmail.com>
2024-04-15 18:41:51 +0530

    .

---
+++ GeeksForGeeks/Count the elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> countElements(vector<int> &a, vector<int> &b, int n, vector<int> &query,
+                              int q) {
+        vector<int> ans;
+        sort(b.begin(), b.end());
+        
+        for(int i=0 ;i<query.size() ;i++){
+            
+            int index = query[i];
+            int val = a[index];
+            int cnt = upper_bound(b.begin(), b.end(), val) - b.begin();
+            
+            ans.push_back(cnt);
+        }
+        
+        return ans;
+    }
+};
