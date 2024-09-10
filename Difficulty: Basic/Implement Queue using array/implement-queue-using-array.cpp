524e7216496a81d55c85ad07d7c8411f5f59f72d
Rakshith Raj <91491093+Rakshith-Raj08@users.noreply.github.com>
2024-09-10 19:51:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement Queue using array/implement-queue-using-array.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+};
+
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* 
+
+The structure of the class is
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+ */
+
+//Function to push an element x in a queue.
+void MyQueue :: push(int x){
+    arr[ rear ] = x;//arr[rear] is what we are pushing, after that move rear
+    rear++;
+}
+
+int MyQueue :: pop()
+{   
+    if(front==rear) return -1;//if front and rear at the same value then -1
+    int popped = arr[ front ];//else front is answer, and move front
+    front++;
+    return popped;
+}
